efbf370628323a8a330ce2390b603f61f78b8816
Chester <chester.htooaunglinn@gmail.com>
2021-07-12 00:02:30 +0630

    feat: add mId field

--- src/app/models/Markers.ts
+++ src/app/models/Markers.ts
@@ -1,5 +1,6 @@
 import { Schema, Document, model } from 'mongoose';
 import shortid from 'shortid';
+import autoIncrement from 'mongoose-auto-increment';
 
 type MarkerProps = {
   title: string;
@@ -8,32 +9,46 @@ type MarkerProps = {
   latitude: number;
   longitude: number;
   uniqueId: string;
+  mid: number;
 } & Document;
 
-const MarkerSchema = new Schema({
-  title: {
-    type: String,
-    required: true
+const MarkerSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    address: {
+      type: String,
+      required: true
+    },
+    numbers: {
+      type: Schema.Types.Array,
+      required: true
+    },
+    latitude: {
+      type: Number,
+      required: true
+    },
+    longitude: {
+      type: Number,
+      required: true
+    },
+    uniqueId: {
+      type: String
+    },
+    mId: {
+      type: Number
+    }
   },
-  address: {
-    type: String,
-    required: true
-  },
-  numbers: {
-    type: Schema.Types.Array,
-    required: true
-  },
-  latitude: {
-    type: Number,
-    required: true
-  },
-  longitude: {
-    type: Number,
-    required: true
-  },
-  uniqueId: {
-    type: String
-  }
+  { timestamps: true }
+);
+
+MarkerSchema.plugin(autoIncrement.plugin, {
+  model: 'Marker',
+  field: 'mId',
+  startAt: 1,
+  incrementBy: 1
 });
 
 MarkerSchema.pre<MarkerProps>('save', function (next) {
